ca505f4f514d171626e8cb19a080cd48d9e2c59f
Xhan Haziraj <xhan19111@gmail.com>
2020-09-01 15:44:37 +0200

    no message

--- Gaming-Library/Gaming-Library/Form1.Designer.cs
+++ Gaming-Library/Gaming-Library/Form1.Designer.cs
@@ -27,39 +27,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Syncfusion.Windows.Forms.Tools.CustomImageCollection customImageCollection1 = new Syncfusion.Windows.Forms.Tools.CustomImageCollection();
-            Syncfusion.Windows.Forms.Tools.ResetButton resetButton1 = new Syncfusion.Windows.Forms.Tools.ResetButton();
-            this.ratingControl1 = new Syncfusion.Windows.Forms.Tools.RatingControl();
             this.SuspendLayout();
-            // 
-            // ratingControl1
-            // 
-            this.ratingControl1.ApplyGradientColors = false;
-            this.ratingControl1.Images = customImageCollection1;
-            this.ratingControl1.Location = new System.Drawing.Point(50, 65);
-            this.ratingControl1.Name = "ratingControl1";
-            this.ratingControl1.ResetButton = resetButton1;
-            this.ratingControl1.Size = new System.Drawing.Size(148, 27);
-            this.ratingControl1.TabIndex = 0;
-            this.ratingControl1.Text = "ratingControl1";
-            this.ratingControl1.ThemeName = "Default";
-            this.ratingControl1.Value = 3F;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 399);
-            this.Controls.Add(this.ratingControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Syncfusion.Windows.Forms.Tools.RatingControl ratingControl1;
     }
 }
 
--- Gaming-Library/Gaming-Library/Form1.cs
+++ Gaming-Library/Gaming-Library/Form1.cs
@@ -21,5 +21,10 @@ namespace Gaming_Library
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
