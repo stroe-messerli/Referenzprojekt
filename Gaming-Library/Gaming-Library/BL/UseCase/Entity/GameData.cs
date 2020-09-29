7e4c9311e523c9b789f8cf880c3a8bd65042ad1f
Xhan Haziraj <xhan19111@gmail.com>
2020-09-29 12:13:15 +0200

    added ui-components

---
+++ Gaming-Library/Gaming-Library/BL/UseCase/Entity/GameData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaming_Library.BL.UseCase.Entity
+{
+    struct GameData
+    {
+        public String Title;
+        public String Genre;//vllt. eigener Type mit Eigenschaften
+        public DateTime YearOfPublication;
+        public String ImagePath;
+    }
+}
--- Gaming-Library/Gaming-Library/Program.cs
+++ Gaming-Library/Gaming-Library/Program.cs
@@ -16,7 +16,7 @@ namespace Gaming_Library
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
