c37de9427feb5f95b6cc154bed63167e2d5b98f8
Xhan Haziraj <xhan19111@gmail.com>
2020-11-27 15:40:19 +0100

    added classes for handling of Start-Command (Composition of executable Game-path) project-file for all previous commits

---
+++ Gaming-Library/Gaming-Library/BL/UseCase/Interactor/PathComposer/SteamGamePathComposer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaming_Library.BL.UseCase.Interactor.PathComposer
+{
+    class SteamGamePathComposer : IGamePathComposer
+    {
+        public static IGamePathComposer Create()
+        {
+            return new SteamGamePathComposer();
+        }
+        private SteamGamePathComposer()
+        {
+        }
+
+        public string ComposeExecutablePath(Model interactorModel)
+        {
+            return "steam://rungameid/" + interactorModel.Game.SteamId.ToString();
+        }
+    }
+}
