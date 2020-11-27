a616c7551149863626e0aea3459cdd31137ab4a0
Xhan Haziraj <xhan19111@gmail.com>
2020-11-27 15:37:47 +0100

    added requests and command-classes for Delete/Start/OpenProperties actions

---
+++ Gaming-Library/Gaming-Library/BL/UseCase/Interactor/Commands/Delete.cs
@@ -0,0 +1,38 @@
+﻿using Gaming_Library.BL.UseCase.InputPort.Requests;
+
+namespace Gaming_Library.BL.UseCase.Interactor.Commands
+{
+    class Delete : ICommand
+    {
+        private Injector _injector;
+        public sealed class Injector
+        {
+            public UseCase.Interactor.Model model;
+
+        };
+
+        public static ICommand Create(Injector injector)
+        {
+            return new Delete(injector);
+        }
+
+        private Delete(Injector injector)
+        {
+            _injector = injector;
+        }
+
+
+        public void Do(IRequest request)
+        {
+
+        }
+
+        public int GetId()
+        {
+            return GetType().GetHashCode();
+        }
+
+
+    }
+}
+
---
+++ Gaming-Library/Gaming-Library/BL/UseCase/Interactor/Commands/Start.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gaming_Library.BL.UseCase.Entity.Types;
+using Gaming_Library.BL.UseCase.InputPort.Requests;
+using Gaming_Library.BL.UseCase.Interactor.PathComposer;
+
+namespace Gaming_Library.BL.UseCase.Interactor.Commands
+{
+    class Start : ICommand
+    {
+        private Injector _injector;
+        public sealed class Injector
+        {
+            public UseCase.Interactor.Model InteractorModel;
+
+        };
+
+        public void Do(IRequest request)
+        {
+            var pathtype = _injector.InteractorModel.Game.SteamId != null ? PathTypes.STEAM : PathTypes.NON_STEAM;
+            var pathComposer = GamePathComposerFactory.CreatePathComposer(pathtype);
+            var execPath = pathComposer.ComposeExecutablePath(_injector.InteractorModel);
+            Process.Start(execPath);
+        }
+
+        public int GetId()
+        {
+            return GetType().GetHashCode();
+        }
+    }
+}
